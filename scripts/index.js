5a8d3d297dc0c32ea8bb4c9788ab452a9c7f437a
Daria Ovchinnikova <daria_ovch@mail.ru>
2020-07-16 01:52:07 +0200

    hotfixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,30 @@
-const openPopupButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close-button');
-const form = document.querySelector('.popup__container');
-const submitPopupButton = document.querySelector('.popup__save-button');
-let inputName = document.querySelector('.popup__name');
-let inputOccupation = document.querySelector('.popup__occupation');
-let profileName = document.querySelector('.profile__name');
-let profileOccupation = document.querySelector('.profile__occupation');
+const openPopupButton = document.querySelector(".profile__edit-button");
+const popup = document.querySelector(".popup");
+const closePopupButton = document.querySelector(".popup__close-button");
+const form = document.querySelector(".popup__container");
+const submitPopupButton = document.querySelector(".popup__save-button");
+let inputName = document.querySelector(".popup__input_name");
+let inputOccupation = document.querySelector(".popup__input_occupation");
+let profileName = document.querySelector(".profile__name");
+let profileOccupation = document.querySelector(".profile__occupation");
 
-function openPopup() {
-    popup.classList.add('popup_opened');
-}
-
-function closePopup() {
-    popup.classList.remove('popup_opened');
+function togglePopup() {
+    if (popup.classList.contains("popup_opened") === false) {
+        inputName.value = profileName.textContent;
+        inputOccupation.value = profileOccupation.textContent;
+        popup.classList.add("popup_opened");
+    } else {
+        popup.classList.remove("popup_opened");
+    }
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileOccupation.textContent = inputOccupation.value;
-    submitPopupButton.addEventListener('click', closePopup);
+    togglePopup();
 }
 
-function noSavePopup() {
-    inputName.value = profileName.textContent;
-    inputOccupation.value = profileOccupation.textContent;
-    closePopup();
-}
-
-openPopupButton.addEventListener('click', openPopup);
-closePopupButton.addEventListener('click', noSavePopup);
-form.addEventListener('click', formSubmitHandler);
+openPopupButton.addEventListener("click", togglePopup);
+closePopupButton.addEventListener("click", togglePopup);
+form.addEventListener("submit", formSubmitHandler);
