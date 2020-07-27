380a84b6911dbdcf3c6792e1209979ffc060c4c0
Daria Ovchinnikova <daria_ovch@mail.ru>
2020-07-27 19:17:36 +0200

    hotfixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 //Popup windows
-const popup = document.querySelector('.popup');
 const editProfilePopup = document.querySelector('.popup_type_edit-profile');
 const addCardPopup = document.querySelector('.popup_type_add-card');
 const showCardPopup = document.querySelector('.popup_type_show-card');
@@ -17,21 +16,21 @@ const closeShowCardPopupButton = showCardPopup.querySelector('.popup__close-butt
 const editForm = editProfilePopup.querySelector('.popup__form');
 const addCardForm = addCardPopup.querySelector('.popup__form');
 
-let inputName = document.querySelector('.popup__input_name');
-let inputOccupation = document.querySelector('.popup__input_occupation');
-let profileName = document.querySelector('.profile__name');
-let profileOccupation = document.querySelector('.profile__occupation');
+const inputName = document.querySelector('.popup__input_name');
+const inputOccupation = document.querySelector('.popup__input_occupation');
+const profileName = document.querySelector('.profile__name');
+const profileOccupation = document.querySelector('.profile__occupation');
 
-let inputPlace = document.querySelector('.popup__input_place');
-let inputLink = document.querySelector('.popup__input_link');
+const inputPlace = document.querySelector('.popup__input_place');
+const inputLink = document.querySelector('.popup__input_link');
 
 //Card template and list
 const cardTemplate = document.querySelector('.cards__template').content.querySelector('.cards__item');
 const grid = document.querySelector('.cards__grid');
 
 //showImagePopup content
-let popupImage = document.querySelector('.popup__image');
-let popupHeading = document.querySelector('.popup__image-caption');
+const popupImage = document.querySelector('.popup__image');
+const popupHeading = document.querySelector('.popup__image-caption');
 
 //Open&close popup window
 function togglePopup(popup) {
@@ -63,7 +62,7 @@ function createCard(data) {
 
     const cardImage = cardElement.querySelector('.cards__image');
     const cardHeading = cardElement.querySelector('.cards__heading');
-    const cardLikeButton = cardElement.querySelector('.cards__like-button')
+    const cardLikeButton = cardElement.querySelector('.cards__like-button');
     const cardDeleteButton = cardElement.querySelector('.cards__delete-button');
     
     cardLikeButton.addEventListener('click', (evt) => {
@@ -72,7 +71,7 @@ function createCard(data) {
 
     cardDeleteButton.addEventListener('click', (evt) => {
         evt.target.closest('.cards__item').remove();
-    })
+    });
     
     cardImage.src = data.link;
     cardHeading.textContent = data.name;
@@ -106,7 +105,7 @@ openAddCardPopupButton.addEventListener('click', () => {
 });
 
 closeEditProfilePopupButton.addEventListener('click', () => {
-    togglePopup(editProfilePopup)
+    togglePopup(editProfilePopup);
 });
 closeAddCardPopupButton.addEventListener('click', () => {
     togglePopup(addCardPopup);
@@ -120,33 +119,33 @@ editForm.addEventListener('submit', formSubmitHandler);
 addCardForm.addEventListener('submit', addCardSubmitHandler);
 
 
-//Cards grid array
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
+// Cards grid array
+// const initialCards = [
+//     {
+//         name: 'Архыз',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+//     },
+//     {
+//         name: 'Челябинская область',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+//     },
+//     {
+//         name: 'Иваново',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+//     },
+//     {
+//         name: 'Камчатка',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+//     },
+//     {
+//         name: 'Холмогорский район',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+//     },
+//     {
+//         name: 'Байкал',
+//         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+//     }
+// ];
 
 initialCards.forEach((data) => {
     renderCard(data);
